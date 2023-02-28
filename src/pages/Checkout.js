4f240a19b752086e85b3b1f2a8a72aec6df476e2
D <laivanson.dev@gmail.com>
2023-02-28 23:26:57 +0700

    .

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { BiArrowBack } from "react-icons/bi";
 import Container from "../components/Container";
