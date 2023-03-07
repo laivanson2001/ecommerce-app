35534b6a54f4ee1f0b9b576ba52d48e03f3c8aea
D <laivanson.dev@gmail.com>
2023-03-07 08:20:05 +0700

    Admin Login , Save Address , Get Wishlist

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -234,6 +234,66 @@ const resetPassword = asyncHandler(async (req, res) => {
 	res.json(user);
 });
 
+const loginAdmin = asyncHandler(async (req, res) => {
+	const { email, password } = req.body;
+	const findAdmin = await userModel.findOne({ email });
+	if (findAdmin.role !== "admin") throw new Error("Not Authorised");
+	if (findAdmin && (await findAdmin.isPasswordMatched(password))) {
+		const refreshToken = await generateRefreshToken(findAdmin?._id);
+		const updateuser = await userModel.findByIdAndUpdate(
+			findAdmin.id,
+			{
+				refreshToken: refreshToken,
+			},
+			{ new: true }
+		);
+		res.cookie("refreshToken", refreshToken, {
+			httpOnly: true,
+			maxAge: 72 * 60 * 60 * 1000,
+		});
+		res.json({
+			_id: findAdmin?._id,
+			firstname: findAdmin?.firstname,
+			lastname: findAdmin?.lastname,
+			email: findAdmin?.email,
+			mobile: findAdmin?.mobile,
+			token: generateToken(findAdmin?._id),
+		});
+	} else {
+		throw new Error("Invalid Credentials");
+	}
+});
+
+const getWishlist = asyncHandler(async (req, res) => {
+	const { _id } = req.user;
+	try {
+		const findUser = await userModel.findById(_id).populate("wishlist");
+		res.json(findUser);
+	} catch (error) {
+		throw new Error(error);
+	}
+});
+
+const saveAddress = asyncHandler(async (req, res, next) => {
+	const { _id } = req.user;
+	validateMongoDbId(_id);
+
+	try {
+		const updatedUser = await userModel.findByIdAndUpdate(
+			_id,
+			{
+				address: req?.body?.address,
+			},
+			{
+				new: true,
+			}
+		);
+		res.json(updatedUser);
+	} catch (error) {
+		throw new Error(error);
+	}
+});
+
 module.exports = {
 	createUser,
 	loginUser,
@@ -248,4 +308,7 @@ module.exports = {
 	updatePassword,
 	forgotPasswordToken,
 	resetPassword,
+	loginAdmin,
+	getWishlist,
+	saveAddress,
 };
--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -13,6 +13,9 @@ const {
 	updatePassword,
 	forgotPasswordToken,
 	resetPassword,
+	loginAdmin,
+	getWishlist,
+	saveAddress,
 } = require("../controller/userController");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const authRouter = express.Router();
@@ -20,14 +23,17 @@ const authRouter = express.Router();
 authRouter.get("/all-users", getAllUser);
 authRouter.get("/refresh", handleRefreshToken);
 authRouter.get("/logout", logout);
+authRouter.get("/wishlist", authMiddleware, getWishlist);
 authRouter.get("/:id", authMiddleware, isAdmin, getUserById);
 
 authRouter.post("/register", createUser);
 authRouter.post("/login", loginUser);
 authRouter.post("/forgot-password-token", forgotPasswordToken);
+authRouter.post("/admin-login", loginAdmin);
 
 authRouter.put("/edit-user", authMiddleware, updatedUser);
 authRouter.put("/password", authMiddleware, updatePassword);
+authRouter.put("/save-address", authMiddleware, saveAddress);
 authRouter.put("/block-user/:id", authMiddleware, isAdmin, blockUser);
 authRouter.put("/unblock-user/:id", authMiddleware, isAdmin, unblockUser);
 authRouter.put("/reset-password/:token", resetPassword);
